0d6aa2f4ca386dfdc4197b6131badc69785dfc74
hokuto-kato <hokuto15@gmail.com>
2022-12-05 00:15:45 +0900

    5.10 修正

--- 5章/main.js
+++ 5章/main.js
@@ -1,3 +1,3 @@
-const array = [1, 2, 3, 4, 5]
-console.log(array.length)
-console.log(array)
+const array = []
+const propDesc = Reflect.getOwnPropertyDescriptor(array, 'length')
+console.log(propDesc.enumerable)
