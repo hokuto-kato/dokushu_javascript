dc6a6750fbf3e23e6130eba546aaccbbf7fb3e8d
hokuto-kato <hokuto15@gmail.com>
2022-12-04 23:56:54 +0900

    5.10まで

--- 5章/main.js
+++ 5章/main.js
@@ -1,14 +1,3 @@
-const obj = {
-	prop1: 10,
-	prop2: 20,
-	skip: 20, // ───この値は加算しない
-	prop3: 23,
-	prop4: 47
-}
-let sum = 0
-for (const key in obj) {
-	if (key !== 'skip') {
-		sum += obj[key]
-	}
-}
-console.log(sum)
+const array = [1, 2, 3, 4, 5]
+console.log(array.length)
+console.log(array)
