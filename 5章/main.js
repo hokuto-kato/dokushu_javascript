673095544a5145458974f245ed870eaa1c5bcee9
hokuto-kato <hokuto15@gmail.com>
2022-12-11 23:59:12 +0900

    5.11まで

--- 5章/main.js
+++ 5章/main.js
@@ -1,3 +1,6 @@
-const array = []
-const propDesc = Reflect.getOwnPropertyDescriptor(array, 'length')
-console.log(propDesc.enumerable)
+const array = [10, '文字列', 20, true, 23, 47]
+let num = 0
+for (const value of array) {
+	if (typeof value === 'number') num += value
+}
+console.log(num)
