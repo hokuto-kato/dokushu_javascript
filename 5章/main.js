4c38689f01c5e2e939c7c71b122ded661c03b021
hokuto-kato <hokuto15@gmail.com>
2022-12-15 23:59:40 +0900

    5.12まで

--- 5章/main.js
+++ 5章/main.js
@@ -1,6 +1,17 @@
-const array = [10, '文字列', 20, true, 23, 47]
-let num = 0
-for (const value of array) {
-	if (typeof value === 'number') num += value
+const obj = {
+	prop1: 10,
+	prop2: '文字列',
+	prop3: 20,
+	skip: 20,
+	prop4: true,
+	prop5: 23,
+	prop6: 47
 }
-console.log(num)
+let sum = 0 //sumに値を加算していく
+
+for (let objElement of Object.entries(obj)) {
+	if (objElement[0] !== "skip" && typeof objElement[1] === "number"){
+		sum += objElement[1]
+	}
+}
+console.log(sum)
