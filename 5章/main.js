e8731e022ed4495242e436c0976d85e5fb831787
hokuto-kato <hokuto15@gmail.com>
2022-12-19 05:26:39 +0900

    5章理解度チェックまで

--- 5章/main.js
+++ 5章/main.js
@@ -1,19 +1,5 @@
-const obj = {
-	prop1: 10,
-	prop2: '文字列',
-	prop3: 20,
-	skip: 20,
-	prop4: true,
-	prop5: 23,
-	prop6: 47
+const capitals = {
+	日本: '東京',
+	アメリカ: 'ワシントンD.C.',
+	イギリス: 'ロンドン'
 }
-let sum = 0 //sumに値を加算していく
-
-for (let objElement of Object.entries(obj)) {
-	const key = objElement[0]
-	const value = objElement[1]
-	if (key !== "skip" && typeof value === "number"){
-		sum += value
-	}
-}
-console.log(sum)
