5b901dd637111ee5c58bb85a994dc3733dd4bb2a
hokuto-kato <hokuto15@gmail.com>
2022-12-22 23:54:06 +0900

    練習問題6.6まで

--- 6章/main.js
+++ 6章/main.js
@@ -1,5 +1,12 @@
-hello()
-
-const hello = () => {
-	console.log('hello') // Uncaught ReferenceError
+const obj1 = { num: 3 }
+const obj2 = { num: 3 }
+let num = 3
+function fn (object1Arg, object2Arg, numberArg) {
+	object1Arg = { num: 5 }
+	object2Arg.num = 5
+	numberArg = 5
 }
+fn(obj1, obj2, num)
+console.log(obj1.num) // 3
+console.log(obj2.num) // 5
+console.log(num) // 3
