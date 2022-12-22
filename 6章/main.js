8c8d717dabce4e3614531e5e53f111faacd75047
hokuto-kato <hokuto15@gmail.com>
2022-12-23 08:53:07 +0900

    6章理解度チェックまで

--- 6章/main.js
+++ 6章/main.js
@@ -1,12 +1,15 @@
-const obj1 = { num: 3 }
-const obj2 = { num: 3 }
-let num = 3
-function fn (object1Arg, object2Arg, numberArg) {
-	object1Arg = { num: 5 }
-	object2Arg.num = 5
-	numberArg = 5
+// 1
+const fn1 = (num1, num2) => num1 + num2
+// 2
+const fn2 = num => num * 2
+
+// 3
+const fn3 = () => console.log('HelloWorld')
+
+// 4
+const fn4 = (name) => {
+	console.log('HelloWorld')
+	console.log(`Hello${name}!`)
 }
-fn(obj1, obj2, num)
-console.log(obj1.num) // 3
-console.log(obj2.num) // 5
-console.log(num) // 3
+// 5
+const fn5 = () => ({ name: '独習太郎' })
