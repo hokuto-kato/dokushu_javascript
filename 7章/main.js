a4601dd1aa11418eaf123e5c902036ccbd5dda9c
hokuto-kato <hokuto15@gmail.com>
2022-12-25 23:42:46 +0900

    7.1まで

---
+++ 7章/main.js
@@ -0,0 +1,15 @@
+// 1
+const fn1 = (num1, num2) => num1 + num2
+// 2
+const fn2 = num => num * 2
+
+// 3
+const fn3 = () => console.log('HelloWorld')
+
+// 4
+const fn4 = (name) => {
+	console.log('HelloWorld')
+	console.log(`Hello${name}!`)
+}
+// 5
+const fn5 = () => ({ name: '独習太郎' })
