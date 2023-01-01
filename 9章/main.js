8140cb92a343eba094ca3b5c2cc0a7a12f940ea5
hokuto-kato <hokuto15@gmail.com>
2023-01-01 23:51:40 +0900

    練習問題10.2まで

--- 9章/main.js
+++ 9章/main.js
@@ -1,31 +1,5 @@
-class User {
-	constructor (username) {
-		this.deleted = 0
-		this.username = username
-	}
-	login () {
-		if (this.deleted) {
-			console.log(`${this.username}はログインに失敗しました。`)
-		} else {
-			console.log(`${this.username}はログインに成功しました。`)
-		}
-	}
-}
-
-class AdminUser extends User {
-	constructor (username) {
-		super(username)
-	}
-	deleteUser (user) {
-		if (user instanceof User) {
-			user.deleted = 1
-			console.log(`${user.username}を削除しました。`)
-		} else {
-			throw new Error('Userオブジェクトを引数にする必要があります。')
-		}
-	}
-}
-
-const user = new User('Tom')
-const admin = new AdminUser('Bob')
-admin.deleteUser(user)
+const date = new Date('2022-05-12:03:12:13:333')
+date.setDate(15)
+date.setMonth(7)
+console.log(date)
+console.log(date.getDay()) // 1（月曜日）
