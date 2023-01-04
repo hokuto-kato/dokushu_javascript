2902fc800a1b5007a2b3df55256d08ffd562aad4
hokuto-kato <hokuto15@gmail.com>
2023-01-04 21:47:15 +0900

    10章理解度チェックまで

--- 10章/main.js
+++ 10章/main.js
@@ -1,31 +1,14 @@
-class User {
-	constructor (username) {
-		this.deleted = 0
-		this.username = username
-	}
-	login () {
-		if (this.deleted) {
-			console.log(`${this.username}はログインに失敗しました。`)
-		} else {
-			console.log(`${this.username}はログインに成功しました。`)
-		}
-	}
+let position = { x: 0, y: 0 }
+
+if (localStorage.getItem('position')) {
+	position = JSON.parse(localStorage.getItem('position'))
+	scrollTo(position.x, position.y)
 }
 
-class AdminUser extends User {
-	constructor (username) {
-		super(username)
-	}
-	deleteUser (user) {
-		if (user instanceof User) {
-			user.deleted = 1
-			console.log(`${user.username}を削除しました。`)
-		} else {
-			throw new Error('Userオブジェクトを引数にする必要があります。')
-		}
-	}
-}
-
-const user = new User('Tom')
-const admin = new AdminUser('Bob')
-admin.deleteUser(user)
+setInterval(() => {
+	position.x = scrollX
+	position.y = scrollY
+	const json = JSON.stringify(position)
+	console.log(json)
+	localStorage.setItem('position', json)
+}, 1000)
