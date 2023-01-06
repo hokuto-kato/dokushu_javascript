b2c1719e9270c2d28615fbf83284fe4b2dcb8970
hokuto-kato <hokuto15@gmail.com>
2023-01-06 22:16:07 +0900

    11章理解度チェックまで

--- 11章/main.js
+++ 11章/main.js
@@ -1,4 +1,30 @@
-const chuka = ['八宝菜', '餃子', '回鍋肉', '青椒肉絲']
-const slicedChuka = chuka.slice(1, 4)
-const reversedChuka = slicedChuka.reverse()
-const isHaposai = slicedChuka.includes('八宝菜')
+// Personクラス
+class Person {
+	constructor (fullname, age, gender) {
+		this.fullname = fullname
+		this.age = age
+		this.gender = gender
+	}
+}
+
+// 登場人物
+const taro = new Person('太郎', 18, '男')
+const jiro = new Person('次郎', 15, '男')
+const saburo = new Person('三郎', 10, '男')
+const hanako = new Person('花子', 23, '女')
+const hanayo = new Person('花代', 18, '女')
+// 友達（friends）オブジェクト
+const friends = new Map
+friends.set(taro, new Set([jiro, hanayo]))
+friends.set(hanako, new Set([hanayo, taro, saburo]))
+const taroFriendsSet = friends.get(taro)
+taroFriendsSet.add(hanako)
+const taroFriendsArray = Array.from(taroFriendsSet)
+
+for (const [person, friendSet] of friends) {
+	let friendsName = ''
+	for (const friend of friendSet) {
+		friendsName += `[${friend.fullname}]`
+	}
+	console.log(`私の名前は${person.fullname}です。友達には${friendsName}がいます。`)
+}
